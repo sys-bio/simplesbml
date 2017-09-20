1a330e9d301b9c1e51f51c1ce2cb6becf1b4af1d
0u812 <0u812@users.noreply.github.com>
2017-09-20 12:44:53 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,8 @@ Created on Thu Feb 12 16:19:07 2015
 from setuptools import setup
 
 setup(name='simplesbml',
-      version='1.1',
+      version='1.2.0',
       description='A simplified interface for constructing SBML docs',
-      author='Caroline Cannistra',
-      author_email='carolc24@uw.edu',
-      packages=['simplesbml'],
-      zip_safe=False)
-
-f = open('simplesbml/__init__.py', 'r');
-init = f.read();
-f.close();
-g = open('__init__.py', 'w');
-g.write(init);
-g.close();
+      author='Caroline Cannistra, Kyle Medley',
+      author_email='tellurium-discuss@googlegroups.com',
+      packages=['simplesbml'])
