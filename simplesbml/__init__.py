f4c48af671ca982e0d50bfecfbb4e76adae9ff2a
Herbert Sauro <hsauro@uw.edu>
2020-08-27 15:29:45 -0700

    fixed typo in __init__.py

--- simplesbml/__init__.py
+++ simplesbml/__init__.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 27 23:13:42 2015, 2020
 
-@author: carolc24, Kyle Medle, hsauro
+@author: carolc24, Kyle Medley, hsauro
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
